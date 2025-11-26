64eb69ab41b093885bd6130ccb11ff722f1a8fa4
LouBlu13 <lou.blumenthal@zhdk.ch>
2025-11-26 10:21:59 +0100

    Update script.js

--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -8,7 +8,7 @@ let products = [];
 let cols = 2; // Zwei Spalten
 
 function setup() {
-	createCanvas(windowWith, windowHeight);
+	createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
   textSize(14);
